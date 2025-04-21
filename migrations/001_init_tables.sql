c8df0303695086bdbfd756035781b86e142d0969
Darron Park <darron1217@gmail.com>
2025-04-21 22:11:39 +0900

    feat(migrations): update general_message_histories and slot_message_stats tables to include fork_version and improve comments

--- migrations/001_init_tables.sql
+++ migrations/001_init_tables.sql
@@ -1,11 +1,11 @@
 -- general_message_histories
 CREATE TABLE general_message_histories (
     arrival_time        DateTime,       -- When message was first seen
-    topic_group         String,         -- e.g., voluntary_exit, proposer_slashing, attester_slashing
-    topic               String,         -- e.g., /eth2/6a95a1a9/voluntary_exit/ssz_snappy
+    fork_version        String,         -- e.g., 6a95a1a9
+    topic               String,         -- e.g., voluntary_exit
     node_region         String,         -- e.g., "CENTRAL_EUROPE", "NORTH_AMERICA", "EAST_ASIA", etc.
     node_alias          String,         -- Alias of the watcher node
-    node_peer_count     UInt32,         -- Peercount of the watcher node
+    node_peer_count     UInt32,         -- Peer Count of the watcher node
     message_id          String,         -- Unique message identifier
     size_bytes          UInt32,         -- Message size in bytes
 )
@@ -17,10 +17,11 @@ ORDER BY (arrival_time, topic_group, topic, node_region, node_alias);
 CREATE TABLE slot_message_stats (
     slot                UInt64,         -- Beacon slot number (6-second interval)
     topic_group         String,         -- e.g., block, attestation, blob
-    topic               String,         -- e.g., /eth2/6a95a1a9/beacon_attestation_10/ssz_snappy
+    fork_version        String,         -- e.g., 6a95a1a9
+    topic               String,         -- e.g., beacon_attestation_10
     node_region         String,         -- e.g., "CENTRAL_EUROPE", "NORTH_AMERICA", "EAST_ASIA", etc.
     node_alias          String,         -- Alias of the watcher node
-    node_peer_count     UInt32,         -- Peercount of the watcher node
+    node_peer_count     UInt32,         -- Peer Count of the watcher node
     message_id          String,         -- Unique message identifier
     slot_start_time     DateTime,       -- Corresponding timestamp for slot
     first_arrival_time  DateTime,       -- When message was first seen
